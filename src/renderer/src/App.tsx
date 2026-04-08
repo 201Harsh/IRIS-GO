befbc4d9fdd120b92cad61a560aec238de76fc6c
Harsh <pandeygautam039@gmail.com>
2026-04-08 10:36:35 +0530

    feat: initialize App component with basic layout and styling

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div>App</div>
+  return (
+    <>
+      <div className="h-screen w-screen bg-sky-300">
+        <h1 className="text-3xl font-bold underline">IRIS GO</h1>
+      </div>
+    </>
+  )
 }
 
 export default App
