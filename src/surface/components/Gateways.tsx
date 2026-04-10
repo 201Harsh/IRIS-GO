b993655052ec8b7d60273f35f80531b343233c66
Harsh <pandeygautam039@gmail.com>
2026-04-10 21:36:21 +0530

    feat: add Gateways component for managing application connections

---
+++ src/surface/components/Gateways.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Gateways() {
+  return (
+    <div className="flex-1 flex items-center justify-center p-6 text-white/50 font-mono tracking-widest">
+      [ GATEWAYS MODULE: APP CONNECTIONS LOADING... ]
+    </div>
+  );
+}
