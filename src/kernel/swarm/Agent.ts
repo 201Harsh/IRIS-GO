71968064c0f061946c2bff8da84bf0f92e9edf0c
Harsh <pandeygautam039@gmail.com>
2026-04-11 20:31:33 +0530

    feat: implement IrisGoAgent using deepagents with Gemini 3.1 flash-lite

--- src/kernel/swarm/Agent.ts
+++ src/kernel/swarm/Agent.ts
@@ -1,19 +1,27 @@
 import { createDeepAgent } from 'deepagents';
+import z from 'zod';
 
 const CodingInstructions = `you are IRSI an AI assistant for IRIS GO which will help user in Coding Related Tasks`;
 const apiKey = process.env.GOOGLE_API_KEY as string;
 
+const contextSchema = z.object({
+  apiKey: z.string(),
+});
+
 const agent = createDeepAgent({
   model: 'google-genai:gemini-3.1-flash-lite-preview',
   systemPrompt: CodingInstructions,
-  
+  contextSchema,
 });
 
 export const IrisGoAgent = async ({ prompt }: { prompt: string }) => {
   try {
-    const result = await agent.invoke({
-      messages: [{ role: 'user', content: prompt }],
-    });
+    const result = await agent.invoke(
+      {
+        messages: [{ role: 'user', content: prompt }],
+      },
+      { context: { apiKey } }
+    );
     const responseResult = result.messages[result.messages.length - 1].content;
     console.log(responseResult);
     return responseResult;
