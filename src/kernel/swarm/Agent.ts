a0b3ff2cc939c6162fa5b3ec9d1513fe3e0896fa
Harsh <pandeygautam039@gmail.com>
2026-04-11 20:38:32 +0530

    feat: implement IrisGoAgent using deepagents and add dotenv dependency

--- src/kernel/swarm/Agent.ts
+++ src/kernel/swarm/Agent.ts
@@ -1,5 +1,6 @@
+import 'dotenv/config';
 import { createDeepAgent } from 'deepagents';
-import z from 'zod';
+import { z } from 'zod';
 
 const CodingInstructions = `you are IRSI an AI assistant for IRIS GO which will help user in Coding Related Tasks`;
 const apiKey = process.env.GOOGLE_API_KEY as string;
@@ -17,13 +18,10 @@ const agent = createDeepAgent({
 export const IrisGoAgent = async ({ prompt }: { prompt: string }) => {
   try {
     const result = await agent.invoke(
-      {
-        messages: [{ role: 'user', content: prompt }],
-      },
+      { messages: [{ role: 'user', content: prompt }] },
       { context: { apiKey } }
     );
     const responseResult = result.messages[result.messages.length - 1].content;
-    console.log(responseResult);
     return responseResult;
   } catch (error) {
     console.log(error);
